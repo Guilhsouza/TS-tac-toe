d0efb39f2a5560040bd65d71b25047e15b6f9cce
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-10 20:08:04 -0300

    feat(addEvents.ts) adicionando função que insere eventos nos botões

---
+++ src/utils/addEvents.ts
@@ -0,0 +1,13 @@
+import clickToPlay from "../script/clickToPlay.js"
+
+export default function addEvents() {
+    document.getElementById('element1')?.addEventListener('click', clickToPlay)
+    document.getElementById('element2')?.addEventListener('click', clickToPlay)
+    document.getElementById('element3')?.addEventListener('click', clickToPlay)
+    document.getElementById('element4')?.addEventListener('click', clickToPlay)
+    document.getElementById('element5')?.addEventListener('click', clickToPlay)
+    document.getElementById('element6')?.addEventListener('click', clickToPlay)
+    document.getElementById('element7')?.addEventListener('click', clickToPlay)
+    document.getElementById('element8')?.addEventListener('click', clickToPlay)
+    document.getElementById('element9')?.addEventListener('click', clickToPlay)
+}
