aaa24e7b8faa9d5254e5a8078134da2b49eb62e0
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-10 20:11:06 -0300

    feat(playAgain.ts) adicionando botão para jogar novamente

---
+++ src/script/playAgain.ts
@@ -0,0 +1,30 @@
+import addEvents from "../utils/addEvents.js"
+
+export default function playAgain() {
+    const message = document.getElementById('message') as HTMLParagraphElement
+    const playAgain = document.getElementById('play-again')
+    const playAgainBtn = document.createElement('button')
+
+    playAgainBtn.innerText = 'Jogar Novamente'
+    playAgainBtn.classList.add('play-again-btn')
+
+    playAgain?.appendChild(playAgainBtn)
+
+    playAgainBtn.addEventListener('click', () => {
+        const theGame = document.querySelector('.the-game');
+
+        theGame?.querySelectorAll('.elements').forEach((element) => {
+            element.textContent = ''
+            element.classList.remove('Xcolor')
+            element.classList.remove('Ocolor')
+        });
+
+        message.classList.remove('win')
+        message.classList.remove('tie')
+        message.innerHTML = 'O jogador que selecionou o <b>X</b> começa!'
+
+        addEvents()
+        playAgain?.removeChild(playAgainBtn)
+    })
+
+}
