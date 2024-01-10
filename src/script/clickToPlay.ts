1ce3d835c32ded006f2707abe2196e6c8a2e7787
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-10 20:07:03 -0300

    ci(clickToPlay.ts) alterando pastas e nomes para melhorar desenvolvimento do projeto

---
+++ src/script/clickToPlay.ts
@@ -0,0 +1,32 @@
+import playerWin from "./playerWin.js";
+
+let value = 'X';
+
+export default function clickToPlay(ev: Event): void {
+    const element = (ev.target as HTMLInputElement);
+    const message = document.getElementById('message');
+    const player1 = (document.getElementById('player1') as HTMLInputElement).value;
+    const player2 = (document.getElementById('player2') as HTMLInputElement).value;
+
+    if (!player1 || !player2) {
+        return alert('Insira o nome dos jogadores para poder jogar!')
+    }
+
+    if (!element.textContent && value === 'X') {
+        element.textContent = value;
+        element.classList.add('Xcolor');
+        (message as HTMLParagraphElement).textContent = `É a vez do jogador ${player2} (O)`;
+        value = 'O';
+
+        playerWin();
+    };
+
+    if (!element.textContent && value === 'O') {
+        element.textContent = value;
+        element.classList.add('Ocolor');
+        (message as HTMLParagraphElement).textContent = `É a vez do jogador ${player1} (X)`
+        value = 'X';
+
+        playerWin();
+    };
+};
