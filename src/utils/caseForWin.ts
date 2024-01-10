8765c1190c7b80773a31dc0572c04d747ffdec48
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-10 20:09:40 -0300

    feat(caseForWin.ts) adicionando filtro de vitórias

---
+++ src/utils/caseForWin.ts
@@ -0,0 +1,16 @@
+export default function caseForWin(elements: string[], xOrCircle: string) {
+
+    if (elements[0] === xOrCircle && elements[1] === xOrCircle && elements[2] === xOrCircle
+        || elements[3] === xOrCircle && elements[4] === xOrCircle && elements[5] === xOrCircle
+        || elements[6] === xOrCircle && elements[7] === xOrCircle && elements[8] === xOrCircle
+
+        || elements[0] === xOrCircle && elements[3] === xOrCircle && elements[6] === xOrCircle
+        || elements[1] === xOrCircle && elements[4] === xOrCircle && elements[7] === xOrCircle
+        || elements[2] === xOrCircle && elements[5] === xOrCircle && elements[8] === xOrCircle
+
+        || elements[0] === xOrCircle && elements[4] === xOrCircle && elements[8] === xOrCircle
+        || elements[2] === xOrCircle && elements[4] === xOrCircle && elements[6] === xOrCircle) {
+        return true
+    }
+
+}
