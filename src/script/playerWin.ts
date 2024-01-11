20e3835e6b2eb93b04cf69c589deda6469fc408f
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-11 15:10:48 -0300

    feat(playerWin.ts) adicionando retorno para corrigir o bug de clickToPlay.ts

--- src/script/playerWin.ts
+++ src/script/playerWin.ts
@@ -1,7 +1,7 @@
 import caseForWin from "../utils/caseForWin.js";
 import responseForWin from "../utils/responseForWin.js";
 
-export default function playerWin(): void {
+export default function playerWin(): boolean | undefined {
     const player1 = (document.getElementById('player1') as HTMLInputElement).value;
     const player2 = (document.getElementById('player2') as HTMLInputElement).value;
     const tie = (document.getElementById('tie') as HTMLParagraphElement).innerHTML
@@ -17,19 +17,19 @@ export default function playerWin(): void {
 
     if (caseForWin(elements, 'X')) {
         responseForWin('win', player1);
-        (document.getElementById('player1-victory') as HTMLParagraphElement).innerHTML = `<b>Vitorias do jogador:</b> ${Number(player1victory[player1victory?.length - 1]) + 1}`
-        return
+        (document.getElementById('player1-victory') as HTMLParagraphElement).innerHTML = `<b>Vitorias do jogador (X):</b> ${Number(player1victory[player1victory?.length - 2] + player1victory[player1victory?.length - 1]) + 1}`
+        return true
     };
 
     if (caseForWin(elements, 'O')) {
         responseForWin('win', player2);
-        (document.getElementById('player2-victory') as HTMLParagraphElement).innerHTML = `<b>Vitorias do jogador:</b> ${Number(player2victory[player2victory?.length - 1]) + 1}`
-        return
+        (document.getElementById('player2-victory') as HTMLParagraphElement).innerHTML = `<b>Vitorias do jogador (O):</b> ${Number(player2victory[player2victory?.length - 2] + player2victory[player2victory?.length - 1]) + 1}`
+        return true
     };
 
     if (!elements.includes('')) {
         responseForWin('tie');
-        (document.getElementById('tie') as HTMLParagraphElement).innerHTML = `<b>Empates:</b> ${Number(tie[tie?.length - 1]) + 1}`
-        return
+        (document.getElementById('tie') as HTMLParagraphElement).innerHTML = `<b>Empates:</b> ${Number(tie[tie?.length - 2] + tie[tie?.length - 1]) + 1}`
+        return true
     };
 };
