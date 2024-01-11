468585ef7d9ad0cf241984f28d0cc337988f1c8f
Guilherme Souza <gui.edu0380@gmail.com>
2024-01-11 15:30:13 -0300

    fix(clickToPlay.js) corrigindo validação que estava comentada

--- src/dist/script/clickToPlay.js
+++ src/dist/script/clickToPlay.js
@@ -5,9 +5,9 @@ export default function clickToPlay(ev) {
     const message = document.getElementById('message');
     const player1 = document.getElementById('player1').value;
     const player2 = document.getElementById('player2').value;
-    // if (!player1 || !player2) {
-    //     return alert('Insira o nome dos jogadores para poder jogar!')
-    // }
+    if (!player1 || !player2) {
+        return alert('Insira o nome dos jogadores para poder jogar!')
+    }
     if (!element.textContent && value === 'X') {
         element.textContent = value;
         element.classList.add('Xcolor');
